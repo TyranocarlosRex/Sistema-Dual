48c8ec43a9b9579917f663c929df7677b21e024f
TyranocarlosRex <carloscordovahb@gmail.com>
2025-11-04 22:45:54 -0700

    Revert "Merge pull request #1 from AlanAlberto123/main"
    This reverts commit 54d219d564cfd6dd45ad10d90ebd102e939bf16e, reversing
    changes made to 9081d2fa1e583894eb4f2677a743e911bbaec468.

--- vite.config.js
+++ vite.config.js
@@ -13,11 +13,4 @@ export default defineConfig({
         }),
         react(),
     ],
-    server: {
-        host: '0.0.0.0', // Sigue siendo necesario para que el servidor sea visible
-        port: 5173,      // Asegúrate que el puerto sea el que usa Vite
-        hmr: {
-            host: 'localhost', // Fuerza al navegador a usar 'localhost'
-        }
-    }
 });
